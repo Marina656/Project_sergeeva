5c25617ea9f6aae47e6271e24205df8e6962341d
Marina656 <marinasergeeva53255@gmail.com>
2025-11-15 12:15:01 +0300

    Update №1.py

--- PZ_4/№1.py
+++ PZ_4/№1.py
@@ -1,30 +1,38 @@
 '''
-Дано вещественное число xлое число N  (>0). Найти значение выражения 1+x+x^2/(2!)+...+x^N/(N!)(N=12...N).
-Полученное число является приближенным значением функции exp  в точке Х.
+Дано вещественное число x и целое число N (>0). Найти значение выражения 1+x+x^2/(2!)+...+x^N/(N!)(N=12...N).
+Полученное число является приближенным значением функции exp в точке Х.
 '''
 try:
     X = int(input("Введите число X: "))
     N = int(input("Введите целое число N (>0): "))
 
     summa = 0
+    k = 0
 
-    for k in range(N + 1):
+    while k <= N:
         chislitel = 1
-        for i in range(k):
+        i = 0
+        while i < k:
             chislitel *= -1
+            i += 1
 
         stepen = 2 * k + 1
         X_stepen = 1
-        for i in range(stepen):
-          X_stepen *= X
+        i = 0
+        while i < stepen:
+            X_stepen *= X
+            i += 1
 
         znamenatel = 1
-        for i in range(1, stepen + 1):
+        i = 1
+        while i <= stepen:
             znamenatel *= i
+            i += 1
 
         chlen_ryada = chislitel * X_stepen / znamenatel
         summa += chlen_ryada
+        k += 1
 
     print(f"Приближенное значение sin({X}) = {summa}")
 except ValueError:
-        print("Ошибка: Введите корректные числовые значения")
+    print("Ошибка: Введите корректные числовые значения")
