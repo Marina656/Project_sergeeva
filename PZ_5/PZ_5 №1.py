f8316cfac0c8af67cb04a57d1e40383f9c83e937
Marina656 <marinasergeeva53255@gmail.com>
2025-11-29 10:30:53 +0300

    Update PZ_5 №1.py

--- PZ_5/PZ_5 №1.py
+++ PZ_5/PZ_5 №1.py
@@ -2,21 +2,24 @@
 Составить функцию решения задачи: из заданного числа вычли сумму его цифр.
 Из результата вновь вычли сумму его цифр и т.д. Через сколько таких действий получится нуль?
 """
-number = int(input('Введите число:'))
-def count_steps_to_zero(n):
-    steps = 0
-    current_number = n
+try:
+    number = int(input('Введите число:'))
+    def zero(n):
+        steps = 0
+        current_number = n
 
-    while current_number > 0:
-        num = current_number
-        digit_sum = 0
+        while current_number > 0:
+            num = current_number
+            digit_sum = 0
 
-        while num > 0:
-            digit_sum += num % 10
-            num = num // 10
-        current_number -= digit_sum
-        steps += 1
-    return steps
+            while num > 0:
+                digit_sum += num % 10
+                num = num // 10
+            current_number -= digit_sum
+            steps += 1
+        return steps
 
-result = count_steps_to_zero(number)
-print(f"Через {result} действий получится нуль")
+    result = zero(number)
+    print(f"Через {result} действий получится нуль")
+except ValueError:
+    print('Ошибка! Введите коректные числа.')
