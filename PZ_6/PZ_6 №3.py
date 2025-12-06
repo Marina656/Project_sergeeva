43720cea44efd89e3140bcbf23cbdb770a0e311d
Marina656 <marinasergeeva53255@gmail.com>
2025-12-06 12:57:30 +0300

    Update PZ_6 №3.py

--- PZ_6/PZ_6 №3.py
+++ PZ_6/PZ_6 №3.py
@@ -2,24 +2,17 @@
 Дан список размером N, все элементы которого, кроме последнего, упорядочены по возрастанию,сделать список упорядоченым
 переместив последний элемент на элемент на новую позицию.
 """
-import random
-m = random.randint(5, 10)
-a = []
-for _ in range(m):
-    a.append(random.randint(1, 100))
-a.sort()
-a[-1] = random.randint(1, 100)
-print(f"Исходный список: {a}")
-last = a[-1]
-j = 0
-while j < m - 1:
-    if last < a[j]:
-        k = m - 1
-        while k > j:
-            a[k] = a[k - 1]
-            k -= 1
-        a[j] = last
-        break
-    j += 1
+try:
+        n = input("Введите числа через пробел: ")
+        num = n.split()
+        a = []
+        for num in num:
+            a.append(int(num))
+        print(f"Исходный список: {a}")
+        first_element = a[0]
+        a.pop(0)
+        a.append(first_element)
+        print(f"Новый список: {a}")
 
-print(f"Упорядоченный список: {a}")
+except ValueError:
+    print('Ошибка: введите числа через пробел')
