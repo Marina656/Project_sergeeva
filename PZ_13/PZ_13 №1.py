1cfce780c3f8ecc2c7c759ba6da709f2b1a2e3c1
Marina656 <marinasergeeva53255@gmail.com>
2026-05-01 20:02:46 +0300

    новый файл

---
+++ PZ_13/PZ_13 №1.py
@@ -0,0 +1,12 @@
+'''
+Из исходного текстового файла (pazzl.html) выбрать все html-коды изображений.
+Посчитать их количество.
+'''
+import re
+
+with open('pazzl.html', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+images = re.findall(r'<[Ii][Mm][Gg][^>]*>', text)
+
+print(f"Количество изображений: {len(images)}")
